9ea56bdd9fc92026d3d9f396a2b7d133013633fd
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2019-11-18 21:16:43 +0300

    test(utils): time utils
    fix failing tests

--- client/src/utils/timeUtils.test.ts
+++ client/src/utils/timeUtils.test.ts
@@ -3,50 +3,50 @@ import { getTime24Hours, getTime12hours } from "./timeUtils";
 describe("TimeUtils", () => {
 
     describe("getTime24Hours", () => {
-        it("should return 01:01 when it is 1min passed 1 am", () => {
+        it("should return 22:01 when it is 1min passed 10 pm", () => {
             jest.spyOn(global.Date, "now")
                 .mockImplementationOnce(() =>
-                    new Date("2019-11-18T22:01:00.933Z").valueOf()
+                    new Date("2019-11-18T22:01:00").valueOf()
                 );
 
             const actualTime = getTime24Hours();
 
-            expect(actualTime).toEqual("01:01");
+            expect(actualTime).toEqual("22:01");
         });
 
-        it("should return 13:01 when it is 1min passed 1 pm", () => {
+        it("should return 10:01 when it is 1min passed 10 am", () => {
             jest.spyOn(global.Date, "now")
                 .mockImplementationOnce(() =>
-                    new Date("2019-11-18T10:01:00.933Z").valueOf()
+                    new Date("2019-11-18T10:01:00").valueOf()
             );
 
             const actualTime = getTime24Hours();
 
-            expect(actualTime).toEqual("13:01");
+            expect(actualTime).toEqual("10:01");
         });
     });
 
     describe("getTime12hours", () => {
-        it("should return 12:45pm when it is 45 minutes passed 12 noon", () => {
+        it("should return 9:45am when it is 45 minutes passed 9am", () => {
             jest.spyOn(global.Date, "now")
                 .mockImplementationOnce(() =>
-                    new Date("2019-11-18T09:45:00.933Z").valueOf()
+                    new Date("2019-11-18T09:45:00").valueOf()
             );
 
             const actualTime = getTime12hours();
 
-            expect(actualTime).toEqual("12:45pm");
+            expect(actualTime).toEqual("9:45am");
         });
 
-        it("should return 12:45am when it is 30 minutes passed 12 midnight", () => {
+        it("should return 9:30pm when it is 30 minutes passed 9", () => {
             jest.spyOn(global.Date, "now")
                 .mockImplementationOnce(() =>
-                    new Date("2019-11-18T21:30:00.933Z").valueOf()
+                    new Date("2019-11-18T21:30:00").valueOf()
             );
 
             const actualTime = getTime12hours();
 
-            expect(actualTime).toEqual("12:30am");
+            expect(actualTime).toEqual("9:30pm");
         });
     });
 
